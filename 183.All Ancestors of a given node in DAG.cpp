7578c4c2f5e99e15bc0743e6a4987ae7cc7463f4
Sachin Malik <sachinmalik199821@gmail.com>
2022-03-06 11:36:03 +0530

    DFS + Tricky

---
+++ 183.All Ancestors of a given node in DAG.cpp
@@ -0,0 +1,21 @@
+// Traverse all nodes from a given node and add "given node" to ancestor list of all the further nodes.
+// do this for every node. and we will have our answer.
+
+class Solution {
+public:
+vector<vector<int> > getAncestors(int n, vector<vector<int> >& edges) {
+        vector<vector<int> > ans(n), directChild(n);
+        for(auto& e: edges)
+                directChild[e[0]].push_back(e[1]);
+        for(int i = 0; i < n; i++)
+                dfs(i, i, ans, directChild);
+        return ans;
+}
+void dfs(int x, int curr, vector<vector<int> >& ans, vector<vector<int> >& directChild) {
+        for (auto& ch: directChild[curr])
+                if(ans[ch].size() == 0 || ans[ch].back() != x) {
+                        ans[ch].push_back(x);
+                        dfs(x, ch, ans, directChild);
+                }
+}
+};
