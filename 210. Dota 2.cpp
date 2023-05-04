bf71f6940d39bdd385c36170151edd1ab0fb9918
Sachin Malik <sachinmalik199821@gmail.com>
2023-05-04 13:28:55 +0530

    Queue

---
+++ 210. Dota 2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> dir;
+        queue<int> rad;
+        for(int i=0;i<senate.length();i++)
+        {
+            if(senate[i]=='D')
+                dir.push(i);
+            else
+                rad.push(i);
+            
+        }
+        int s=senate.length();
+        while(true)
+        {
+            if(rad.empty()) return "Dire";
+            if(dir.empty()) return "Radiant";
+            int r=rad.front();  rad.pop();
+            int d=dir.front(); dir.pop();
+            
+            if(r<d)
+                rad.push(r+s);
+            else
+                dir.push(d+s);
+        
+        }
+    }
+};
